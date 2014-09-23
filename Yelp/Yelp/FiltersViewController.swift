1129bff2897aa966e9d8ecc488e64bbf02a4b95c
Tianyu Shi <ts133@duke.edu>
2014-09-22 23:27:43 -0700

    doing filters now

--- Yelp/Yelp/FiltersViewController.swift
+++ Yelp/Yelp/FiltersViewController.swift
@@ -15,13 +15,21 @@ class FiltersViewController: UIViewController, UITableViewDataSource, UITableVie
     var client: YelpClient?
     var tempDictionary: NSDictionary?
     
+    var expanded = [false, false, false, false]
+    var distArray: NSArray = [1, 2, 5, 10]
+    var sortMethodArray: NSArray = ["Best", "Distance", "Average Review"]
+    var catagoryArray: NSArray = ["Active Life", "Arts & Entertainment", "Hotels & Travel", "See All"]
+    var catagoryArrayValue: NSArray = ["active", "arts", "hotelstravel", ""]
+    var catagoryArrayAll: NSArray = ["Active Life", "Arts & Entertainment", "Hotels & Travel", "Local Flavor", "Nightlife"]
+    var catagoryArrayAllValue: NSArray = ["active", "arts", "hotelstravel", "localflavor", "nightlife"]
+    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
         filterTableView.estimatedRowHeight = 80
         filterTableView.rowHeight = UITableViewAutomaticDimension
     }
-    
+
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 4
     }
@@ -44,22 +52,78 @@ class FiltersViewController: UIViewController, UITableViewDataSource, UITableVie
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0: // Distance
+            if(expanded[0]) {
+                return distArray.count+1
+            }
             return 1
         case 1: // SortBy
+            if(expanded[1]) {
+                return sortMethodArray.count+1
+            }
             return 1
         case 2: // Deals
             return 1
         case 3: // Catagories
-            return 4
+            if(expanded[3]) {
+                return catagoryArrayAll.count
+            }
+            return catagoryArray.count
         default:
             return 0
         }
     }
     
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+        cell.textLabel!.textColor = UIColor(red: 0, green: 0.81, blue: 1, alpha: 1)
+        if(indexPath.row == 0) {
+            cell.textLabel!.font = UIFont(name: "Avenir Next", size: 12)
+        }
         
-        return cell
+        switch indexPath.section {
+        case 0:
+            if(expanded[indexPath.section] && indexPath.row != 0) {
+                cell.textLabel!.text = String(distArray[indexPath.row - 1] as Int) + " mi"
+            } else {
+                cell.textLabel!.text = "Select Distance!"
+            }
+
+            return cell
+        case 1:
+            let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+
+//            UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+//            NSString *displaySortMethod = self.settings.sortMethod;
+//            if ([self sectionIsExpanded:indexPath.section]) {
+//                displaySortMethod = [self sortMethodStringArray][indexPath.row];
+//            }
+//            cell.textLabel.text = displaySortMethod;
+            return cell;
+        case 2:
+            let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+
+//            YPSwitchTableViewCell *switchCell = [tableView dequeueReusableCellWithIdentifier:@"SwitchCell" forIndexPath:indexPath];
+//            switchCell.switchControl.onTintColor = [UIColor redColor];
+//            switchCell.switchControl.on = self.settings.deals;
+//            [switchCell.switchControl addTarget:self action:@selector(setDeals:) forControlEvents:UIControlEventValueChanged];
+//            switchCell.switchLabel.text = @"Has Deal";
+//            return switchCell;
+            return cell
+        case 3:
+            let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+
+//            UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+//            if ([self sectionIsExpanded:indexPath.section] || indexPath.row < 3) {
+//                cell.textLabel.text = [self.categories allKeys][indexPath.row];
+//            } else {
+//                cell.textLabel.text = @"More...";
+//            }
+            return cell;
+        default:
+            let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+            return cell
+        }
     }
         
     
